65406b604dd2c427e4eb3123f185a72688fdd3ea
Gustavo Filipe <gutofilipe93@gmail.com>
2020-02-26 20:14:20 -0300

    integração com o firebase

---
+++ js/app.js
@@ -0,0 +1,14 @@
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyBEK3lJ7Ft8mazphVE_BOuKOhFTpcj__HY",
+            authDomain: "curso-fb-d3090.firebaseapp.com",
+            databaseURL: "https://curso-fb-d3090.firebaseio.com",
+            projectId: "curso-fb-d3090",
+            storageBucket: "curso-fb-d3090.appspot.com",
+            messagingSenderId: "762766122149",
+            appId: "1:762766122149:web:502c8715f9f8c3a645c14c",
+            measurementId: "G-F61KL9BXKT"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
